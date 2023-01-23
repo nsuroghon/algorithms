d63e0303ea1dc4597b0e16d009f3236e2bb161d5
niv suroghon <nivsuroghon@nivs-MBP.lan>
2023-01-23 14:53:45 -0500

    pushing tree

---
+++ depthFirstSearch/depthFirstValues.js
@@ -0,0 +1,30 @@
+// const a = new Node('a');
+// const b = new Node('b');
+// const c = new Node('c');
+// const d = new Node('d');
+// const e = new Node('e');
+// const f = new Node('f');
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f
+
+// console.log(treeIncludes(a, "e"))
+
+const a = new Node(1);
+const b = new Node(6);
+const c = new Node(0);
+const d = new Node(3);
+const e = new Node(-6);
+const f = new Node(2);
+const g = new Node(2);
+const h = new Node(2);
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+e.left = g;
+f.right = h;
