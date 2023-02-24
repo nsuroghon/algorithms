1b32c516dcc11e20432e59e731074e825456bb71
niv suroghon <nsuroghon@gmail.com>
2023-02-24 14:25:26 -0500

    refining helper function and posting solution for merge sort alg

--- sortingAlgorithms.js/mergeSort.js
+++ sortingAlgorithms.js/mergeSort.js
@@ -4,9 +4,6 @@
 // Takes two SORTED array and merge together to create a third SORTED array
 function helper(left, right, array) {
     // If left & Right are not already sorted this helper function will not work
-    console.log(left);
-    console.log(right);
-    console.log(array);
     
     const result = [];
 
@@ -14,9 +11,8 @@ function helper(left, right, array) {
     const lengthRight = right.length;
     let i = 0;
     let j = 0;
-    // let k = 0;
 
-    while(i < lengthLeft || j < lengthRight){
+    while(i < lengthLeft && j < lengthRight){
         console.log(left[i], right[i])
         // comparing smallest unpicked in Left with smallest unpicked in Right
         if(left[i] <= right[j]) {
@@ -27,11 +23,16 @@ function helper(left, right, array) {
             result.push(right[j]);
             j = j + 1;
         }
-        
-        // k = k + 1
     }
 
-    return result;
+    // While statement iterates while leftIndex & rightIndex are both less than length, but when one side reaches first
+    // in this case the last two & largest two elements on right array are [7,8]
+    // ...right.slice(j) gets the last elements 7,8 and we include those in the end of our return statement array
+    console.log(...result);
+    console.log(...left.slice(i))
+    console.log(...right.slice(j))
+
+    return [...result, ...left.slice(i), ...right.slice(j)];
 }
 
 console.log(helper([1,2,4,6], [3,5,7,8], [2,4,1,6,8,5,3,7]))
@@ -61,37 +62,47 @@ function mergeSort(array) {
     console.log(array)
     if(array.length <= 1) return array;
 
-
     const mid = Math.floor((array.length) / 2)
-    console.log(mid)
 
     const left = array.slice(0,mid)
     console.log(left)
     const right = array.slice(mid)
-
     console.log(right)
 
-    return mergeSort(left)
-
-    // return merge(mergeSort(left), mergeSort(right))
+    return merge(mergeSort(left), mergeSort(right));
 }
 
-function merge(array1, array2) {
-    const result = []
+function merge(left, right) {
+
+    const result = [];
+
+    let i = 0;
+    let j = 0;
     
-    for(let i = 0; i < array1.length; i++) {
-        if(array1[i] < array2[i]) {
-            result.push(array1[i])
-            result.push(array2[i])
-        } else {
-            result.push(array2[i])
-            result.push(array1[i])
+    // while there are still unpicked elements in left OR right
+    while(i < left.length && j < right.length) {
+        // compare first unpicked in left with first unpicked in right
+        // ... push the lowest of the two elements
+        // & increment pointer at given side
+        if(left[i] <= right[j]) {
+            result.push(left[i]);
+            i += 1;
+        }
+        else {
+            result.push(right[j]);
+            j += 1;
         }
     }
-    return result
+
+    console.log(...result);
+    console.log(...left.slice(i))
+    console.log(...right.slice(j))
+    return [...result, ...left.slice(i), ...right.slice(j)];
 }
 
-console.log(merge([2],[8]))
+console.log(merge([1,4], [2,8]))
+// console.log(merge([1,2,4,6], [3,5,7,8]))
+
 
 // console.log(mergeSort([2,8,5,3,9,4,1,7]))
-console.log(mergeSort([1,4,2,8,345,123,43,32,5643,63,123,43,2,55,1,234,92]))
+// console.log(mergeSort([1,4,2,8,345,123,43,32,5643,63,123,43,2,55,1,234,92]))
